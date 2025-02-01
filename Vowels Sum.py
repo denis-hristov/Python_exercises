69e09b2407556ba0a88d74e95066c8a306e38a7f
Denis Hristov <denis.hristov145@gmail.com>
2025-02-01 18:05:04 +0200

    Add files via upload

---
+++ Vowels Sum.py
@@ -0,0 +1,15 @@
+word = input()
+sum = 0
+for i in range(len(word)):
+    if word[i]=='a':
+        sum+=1
+    elif word[i]=='e':
+        sum+=2
+    elif word[i]=='i':
+        sum+=3
+    elif word[i]=='o':
+        sum+=4
+    elif word[i]=='u':
+        sum+=5
+        
+print(sum)
